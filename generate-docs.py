fe9ffdda7d75f90110b4df8686afff6a9068c80f
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2025-09-07 10:44:50 +0200

    agent frameworks

--- generate-docs.py
+++ generate-docs.py
@@ -6,11 +6,13 @@ dateString = datetime.date.today().strftime('%Y-%m-%d')
 with open('docs/index.html', 'w') as file:
     data = json.load(open('data/data.json'))
     running = json.load(open('data/running.json'))
+    agents = json.load(open('data/agents.json'))
     template = open('templates/template.html').read()
     file.write(template
         .replace('${date}', dateString)
         .replace('"${data}"', json.dumps(data))
         .replace('"${running}"', json.dumps(running))
+        .replace('"${agents}"', json.dumps(agents))
         )
 
 
