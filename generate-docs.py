269379166f670356a9fd5525967524f889213c4c
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2026-07-23 23:39:51 +0200

    new data

--- generate-docs.py
+++ generate-docs.py
@@ -7,12 +7,13 @@ with open('docs/index.html', 'w') as file:
     data = json.load(open('data/data.json'))
     running = json.load(open('data/running.json'))
     agents = json.load(open('data/agents.json'))
+    software_agents = json.load(open('data/software-engineering.json'))
     template = open('templates/template.html').read()
     file.write(template
         .replace('${date}', dateString)
         .replace('"${data}"', json.dumps(data))
         .replace('"${running}"', json.dumps(running))
         .replace('"${agents}"', json.dumps(agents))
+        .replace('"${software_agents}"', json.dumps(software_agents))
         )
-
 
